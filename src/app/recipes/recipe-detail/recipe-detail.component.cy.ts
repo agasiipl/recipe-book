ffd0ab16042b24c0126212c0cbdd6f416e1c8eba
Agnieszka Drabczyk <adrabczyk@sii.pl>
2023-04-06 14:23:05 +0200

    added tests for recipe detail page

--- src/app/recipes/recipe-detail/recipe-detail.component.cy.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.cy.ts
@@ -1,4 +1,4 @@
-describe("Recipe Details", () => {
+describe("Recipe Details page", () => {
   beforeEach(() => {
     const email = Cypress.env("email");
     const password = Cypress.env("password");
@@ -15,37 +15,58 @@ describe("Recipe Details", () => {
     cy.get('[data-cy="login-button"]').should("be.visible");
     cy.get('[data-cy="login-button"]').click();
     cy.url().should("include", "/recipes");
-    cy.get('[data-cy="manage"]').should("be.visible");
-    cy.get('[data-cy="manage"]').click();
-    cy.get('[data-cy="fetch-data"]').should("be.visible");
-    cy.get('[data-cy="fetch-data"]').click();
+    cy.get('[data-cy="add-new-recipe"]').should("be.visible");
+    cy.get('[data-cy="add-new-recipe"]').click();
     cy.wait(500);
+    cy.url().should("eq", "http://localhost:4200/recipes/new");
+    cy.get('[data-cy="add-name"]').type("Spring salad");
+    cy.get('[data-cy="add-img-url"]').type(
+      "https://img.taste.com.au/M8iK-2vC/taste/2016/11/blistered-tomato-and-chicken-couscous-salad-103480-1.jpeg"
+    );
+    cy.get('[data-cy="add-description"]').type("Spring salad with chicken");
+    cy.get('[data-cy="add-ingredient"]').click();
+    cy.wait(500);
+    cy.get('[data-cy="add-seperate-ingredient"]').click();
+    cy.get('[data-cy="add-seperate-ingredient"]').type("Lettuce");
+    cy.get('[data-cy="add-seperate-amount"]').type("1");
+    cy.get('[data-cy="save-recipe"]').should("be.visible");
+    cy.get('[data-cy="save-recipe"]').click();
+    cy.wait(500);
+    cy.url().should("eq", "http://localhost:4200/recipes");
     cy.get('[data-cy="recipe-item-name"]').should("be.visible");
+    cy.get('[data-cy="recipe-item-name"]').click();
+    cy.wait(500);
+
+    // cy.get('[data-cy="manage"]').should("be.visible");
+    // cy.get('[data-cy="manage"]').click();
+    // cy.get('[data-cy="fetch-data"]').should("be.visible");
+    // cy.get('[data-cy="fetch-data"]').click();
+    // cy.wait(500);
+    // cy.get('[data-cy="recipe-item-name"]').should("be.visible");
+    // cy.get('[data-cy="recipe-item-name"]').click();
+    // cy.wait(500);
   });
 
   it("should display recipe details correctly", () => {
-    //  cy.get('.row:nth-child(1) img').should('have.attr', 'src', 'https://example.com/image.jpg'); // check if the recipe image source is correct
-    //   cy.get('.row:nth-child(2) h1').should('contain', 'Recipe Name'); // check if the recipe name is correct
-    //   cy.get('.row:nth-child(4)').should('contain', 'Recipe Description'); // check if the recipe description is correct
-    //  cy.get('.row:nth-child(5) li:nth-child(1)').should('contain', 'Ingredient 1 - 1 cup'); // check if the first ingredient and its amount are correct
-    //  cy.get('.row:nth-child(5) li:nth-child(2)').should('contain', 'Ingredient 2 - 2 tbsp'); // check if the second ingredient and its amount are correct
+    cy.get(".row:nth-child(1) img").should(
+      "have.attr",
+      "src",
+      "https://img.taste.com.au/M8iK-2vC/taste/2016/11/blistered-tomato-and-chicken-couscous-salad-103480-1.jpeg"
+    );
+    cy.get(".row:nth-child(2) h1").should("contain", "Spring salad");
+    cy.get(".row:nth-child(4)").should("contain", "Spring salad with chicken");
+    cy.get(".row:nth-child(5) li:nth-child(1)").should(
+      "contain",
+      "Lettuce - 1"
+    );
   });
 
   it("should contain a Manage Recipe dropdown with Add to Shopping List, Edit Recipe and Delete Recipe options", () => {
-    //cy.get('.row:nth-child(3) .btn-group').should('exist'); // check if Manage Recipe dropdown exists
-    cy.get(".dropdown-toggle").click(); // click the dropdown toggle to open the dropdown menu
-    // cy.get('.dropdown-menu li:nth-child(1)').should('contain', 'Add to Shopping List'); // check if Add to Shopping List option exists
-    // cy.get('.dropdown-menu li:nth-child(2)').should('contain', 'Edit Recipe'); // check if Edit Recipe option exists
-    // cy.get('.dropdown-menu li:nth-child(3)').should('contain', 'Delete Recipe'); // check if Delete Recipe option exists
-    // cy.get('.dropdown-menu li:nth-child(1) a').click(); // click Add to Shopping List option
-    cy.url().should("contain", "/recipes"); // check if the URL navigates to the shopping list page
-    cy.go("back"); // go back to the recipe details page
-    // cy.get('.dropdown-menu li:nth-child(2) a').click(); // click Edit Recipe option
-    // cy.url().should('contain', '/edit'); // check if the URL navigates to the recipe edit page
-    // cy.go('back'); // go back to the recipe details page
-    // cy.get('.dropdown-menu li:nth-child(3) a').click(); // click Delete Recipe option
-    // cy.get('.modal-dialog').should('exist'); // check if the confirmation modal is displayed
-    // cy.get('.modal-footer button:nth-child(2)').click(); // click the confirmation button to delete the recipe
-    // cy.url().should('contain', '/recipes'); // check if the URL navigates back to the recipes page
+    cy.get('[data-cy="manage-recipe-button"]').should("be.visible");
+    cy.get('[data-cy="manage-recipe-button"]').click();
+    cy.wait(500);
+    cy.get('[data-cy="add-to-shopping-list"]').should("be.visible");
+    cy.get('[data-cy="edit-recipe"]').should("be.visible");
+    cy.get('[data-cy="delete-recipe"]').should("be.visible");
   });
 });
